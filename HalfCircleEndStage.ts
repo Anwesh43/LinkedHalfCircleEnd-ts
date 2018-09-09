f4f7cb2d7c7cac54c60b37e39f0b2dfb72491c3b
Anwesh43 <anweshthecool0@gmail.com>
2018-09-09 12:10:15 +0530

    adding HCENode class

--- HalfCircleEndStage.ts
+++ HalfCircleEndStage.ts
@@ -71,3 +71,70 @@ class Animator {
         }
     }
 }
+
+class HCENode {
+    state : State = new State()
+    prev : HCENode
+    next : HCENode
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new HCENode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : HCENode {
+        var curr : HCENode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = h / (nodes + 1)
+        const size : number = gap / 3
+        context.fillStyle = '#FFC107'
+        context.save()
+        context.translate(w/2, this.i * gap + gap)
+        for(var i = 0; i < 2; i++) {
+            context.save()
+            context.scale(1 - 2 * (i%2), 0)
+            context.save()
+            context.translate((w/2 - size/2) * this.state.scale, 0)
+            context.rotate(Math.PI * this.state.scale)
+            context.beginPath()
+            for (var j = -90; j <= 90; j++) {
+                const x = (size/2) *  Math.cos(j * Math.PI/180), y = (size/2) * Math.sin(j * Math.PI/180)
+                if (j == -90) {
+                    context.moveTo(x, y)
+                } else {
+                    context.lineTo(x, y)
+                }
+            }
+            context.fill()
+            context.restore()
+            context.restore()
+        }
+        context.restore()
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+}
